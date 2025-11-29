3e44161ddc756d1449d8c5e3e0f184cb2bf7b9b4
Takuto Irie <s24c1016ew@s.chibakoudai.jp>
2025-11-29 10:45:01 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2025 Takuto Irie
 # SPDX-License-Identifier: BSD-3-Clause
+
 ng () {
         echo ${1}行目が違うよ
         res=1
@@ -8,18 +9,16 @@ ng () {
 
 res=0
 
-### NOMAL INPUT ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15.0 ] || ng "$LINENO"
+out=$(echo 10 | ./divisors)
+[ "${out}" = "1 2 5 10 " ] || ng "$LINENO"
 
- ### STRANGE INPUT ###
-#out=$(echo あ | ./plus)
-#[ "$?" = 1 ] || ng "$LINENO"
-#[ "${out}" = "" ] || ng "$LINENO"
+out=$(echo あ | ./divisors)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
-#out=$(echo | ./plus)
-#[ "$?" = 1 ] || ng "$LINENO"
-#[ "${out}" = "" ] || ng "$LINENO"
+out=$(echo  | ./divisors)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
